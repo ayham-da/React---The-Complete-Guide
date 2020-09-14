0942702b52db2b65ac4de57a8031d353c0923056
ayham-da <ayham.alaaeddin@gmail.com>
2020-09-14 23:21:45 +0200

    rewrite App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a React App</h1>
+       <p>This is really working!</p>
+       <Person name="Max" age="28" />
+       <Person name="Manu" age="29" >My Hobbies: Racing</Person>
+       <Person name="Stephanie" age="26" />
     </div>
   );
 }
